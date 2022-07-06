76ba764b4e93055c7e987dbddc426fc2575be427
Lily Tran <tran.lily8@gmail.com>
2022-07-06 13:25:47 -0700

    first commit with all necessary files

---
+++ SolarSystemSwiftUI/SolarSystemSwiftUI/SolarSystemSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemSwiftUIApp.swift
+//  SolarSystemSwiftUI
+//
+//  Created by Lily Tran on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
